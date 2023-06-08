0118338805a52a8a45e5f414ddf5d22626541a14
JeanY100 <134954815+JeanY100@users.noreply.github.com>
2023-06-08 13:36:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,7 +89,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-, 'jackfruit')
+
 streamlit.write('Thanks for adding', add_my_fruit)  
 #this will not work correctly, but just go with it for now
 
